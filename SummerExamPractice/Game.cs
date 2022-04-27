31d63c1c260f9bfcf9d12db11dc06cf6c70da18e
Craig Lawson <S00209542@mail.itsligo.ie>
2022-04-27 12:04:19 +0100

    Add project files.

---
+++ SummerExamPractice/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummerExamPractice
+{
+    class Game
+    {
+        public string Name { get; set; }
+
+        public int MetacriticScore  { get; set; }
+
+        public string Description { get; set; }
+
+        public string Platform { get; set; }
+
+        public int Price { get; set; }
+
+        public string Game_Image { get; set; }
+
+        public int DecreasePrice(int discount)
+        {
+            Price -= discount;
+
+            return Price;
+        }
+    }
+}
